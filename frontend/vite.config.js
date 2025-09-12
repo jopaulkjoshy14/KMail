6edde6af791fd647e296694fe690fe9f15bb3b5a
Jopaul K Joshy <jopaulkjoshy14@gmail.com>
2025-09-12 11:48:07 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,11 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/KMail/',  // 👈 IMPORTANT for GitHub Pages
   server: {
     port: 5173,
-  }
+  },
+  base: '/KMail/',   // 👈 important for GitHub Pages
 })
