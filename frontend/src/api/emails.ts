19d131ee6475ea0cd21716fb042d9865f152366b
Jopaul K Joshy <jopaulkjoshy14@gmail.com>
2025-09-13 15:06:23 +0530

    Create emails.ts

---
+++ frontend/src/api/emails.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const API_BASE = "https://kmail.onrender.com/api";
+
+export const fetchInbox = (token: string) =>
+  axios.get(`${API_BASE}/emails/inbox`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+export const fetchSent = (token: string) =>
+  axios.get(`${API_BASE}/emails/sent`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+export const sendEmail = (token: string, data: { recipients: string[], subject: string, content: string }) =>
+  axios.post(`${API_BASE}/emails/send`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+export const clearEmails = (token: string) =>
+  axios.delete(`${API_BASE}/emails/clear`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
