df683334b03dc7b86855c203ace2cea512cd48e9
Jopaul K Joshy <jopaulkjoshy14@gmail.com>
2025-11-08 13:13:50 +0530

    Delete backend directory

--- backend/src/models/Mail.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const MailSchema = new mongoose.Schema({
-  cipher: { type: Buffer, required: true }, // Encrypted message
-  capsule: { type: Buffer, required: true }, // Kyber ciphertext
-  to: { type: String, required: true }, // Recipient username
-  signature: { type: Buffer, required: true } // Dilithium signature of ciphertext
-  // Do NOT store sender or timestamp to minimize metadata
-});
-module.exports = mongoose.model('Mail', MailSchema);
