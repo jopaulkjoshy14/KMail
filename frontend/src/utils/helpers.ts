0dd5ddba2c5a7553f55ddc9e2ca065d7e129a865
Jopaul K Joshy <jopaulkjoshy14@gmail.com>
2025-11-08 13:04:49 +0530

    Delete frontend directory

--- frontend/src/utils/helpers.ts
+++
@@ -1,9 +0,0 @@
-export const validateEmail = (email: string) => {
-  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return re.test(email);
-};
-
-export const formatDate = (dateString: string) => {
-  const date = new Date(dateString);
-  return date.toLocaleString();
-};
