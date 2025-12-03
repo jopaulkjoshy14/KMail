5166b5fc7f3790502a07256258d818a0c9d18ec1
Jopaul K Joshy <jopaulkjoshy14@gmail.com>
2025-12-04 03:59:05 +0530

    Update db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,11 +2,14 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection error:", error);
-    process.exit(1); // Exit so Render knows deployment failed
+    process.exit(1);
   }
 };
 
